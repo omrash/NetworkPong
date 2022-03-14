861b33a3e9082edbc464cc5eebbde0c24f0e86fe
Omar <rashwan.o@northeastern.edu>
2022-03-14 16:37:52 -0400

    Got SDL2 working and implemented basic structs for game logic

--- src/netpong.c
+++ src/netpong.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL.h"
+
+#include "netpong.h"
 
 int main(int argc, char* argv[]) {
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("SDL could not be initialized: %s", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Window* window;
+
+    window = SDL_CreateWindow("SDL2Test", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
+
+    SDL_Event e;
+    int run = 1;
+    while (run) {
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                run = 0;
+            }
+            if (e.type == SDL_KEYDOWN){
+                run = 0;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                run = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return 0;
 
--- src/netpong.h
+++ src/netpong.h
@@ -1,10 +1,16 @@
 typedef struct ball {
-        int x, y;
-        int width, height;
-        int d_x, d_y; 
+    int x, y;
+    int width, height;
+    int d_x, d_y; 
 } ball_t;
 
 typedef struct paddle {
-        int x,y;
-        int w,h;
+    int x,y;
+    int w,h;
 } paddle_t;
+
+typedef struct game_state {
+    int score[2];
+    ball_t* game_ball;
+    paddle_t* l_paddle, r_paddle;
+} game_state_t;
